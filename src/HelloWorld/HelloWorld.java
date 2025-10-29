c32e289cdc1e18b9618450714db5f2c86e5b08f3
z00528jp <z00528jp@INNM5CD4407MN7.energy.local>
2025-10-29 16:18:45 +0100

    Ausgabetext geändert

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world - my name ist Florian Kuefer");
+		System.out.println("hello world - my name ist Norbert");
 
 	}
 
