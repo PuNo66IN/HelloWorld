aba385f3c2935ccd5f8e50d40d8bf2340f2c36ee
z00528jp <z00528jp@INNM5CD4407MN7.fritz.box>
2025-11-04 17:30:39 +0100

    Ausgabetext erneut geändert

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world - my name is Mr. Norbert");
+		System.out.println("hello world - my name is Norbert alias PuNo66");
 
 	}
 
