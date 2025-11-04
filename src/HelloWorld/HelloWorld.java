248e601be6d12a7d1832b17d8a9e292625df1468
z00528jp <z00528jp@INNM5CD4407MN7.fritz.box>
2025-11-04 17:26:43 +0100

    Text noch einmal geändert

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world - my name is Norbert");
+		System.out.println("hello world - my name is Mr. Norbert");
 
 	}
 
